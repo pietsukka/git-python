7e0869d46b5bb15f3962877a5d914f12843eb93c
Juho-Ville <ejuui001@edu.xamk.fi>
2023-09-13 12:10:49 +0300

    added few print commands

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
-print ("Hello world")
-userInput = input("Type something and press enter. ")
-print(userInput)
-print("Jesse")
+#single note.py, Simple note application
+
+
+
+#modules
+import sys
+
+#main funtion
+def main () -> int:
+    #arguments by sys.arg
+    print(sys.argv)
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[1] + sys.args[2])
+    print(22+23)
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a + b)
+
+
+
+    #return value
+
+    return(0)
+#main function definiton
+
+if __name__ == '__main__':
+    sys.exit(main())
